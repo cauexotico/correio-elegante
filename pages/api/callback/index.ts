cd1f28212809e55f7c54d9d421ab4b031532eab2
Khaue Poerner <khaue.p@grupovvale.local>
2022-06-11 18:54:09 -0300

    fix: twillio callback

--- pages/api/callback/index.ts
+++ pages/api/callback/index.ts
@@ -52,5 +52,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         })
     })
 
-    return res.status(200).json('ok');
+    return res.status(200).json({
+        error: false,
+        message: 'Message sent successfully',
+    })
 }
