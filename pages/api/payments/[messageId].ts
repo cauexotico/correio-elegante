b337f622abb2b0bb667f21d0d1c21a6ddb75d9bc
Khaue Poerner <khaue.p@grupovvale.local>
2023-09-19 15:30:42 -0300

    feature: removing login of sending messages

--- pages/api/payments/[messageId].ts
+++ pages/api/payments/[messageId].ts
@@ -27,17 +27,6 @@ async function updatePaymentStatus(req: NextApiRequest, res: NextApiResponse<Obj
                     payment_status: 'approved',
                 },
             })
-
-            if (await sendMessage(req, res)) {
-                const updateMessage = await prisma.message.update({
-                    where: {
-                        id: messageId,
-                    },
-                    data: {
-                        status: 'sent',
-                    },
-                })
-            }
         }
     }
 
@@ -50,36 +39,6 @@ async function getPayment(paymentId: string) {
     return payment.response;
 }
 
-async function sendMessage(req: NextApiRequest, res: NextApiResponse<Object>) {
-    const messageId = req.query.messageId as string | undefined;
-
-    const message = await prisma.message.findUnique({
-        where: { id: messageId },
-    });
-
-    let response;
-
-    await fetch(`${process.env.WPPCONNECT_URL}/send-message`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.WPPCONNECT_TOKEN}`
-        },
-        body: JSON.stringify({
-            'phone': message?.to,
-            'message': `Como é bom ser lembrado por alguém especial! 😍 \nVocê acabou de receber um Correio Elegante. 💘 \nConfira sua mensagem 👇 \n\n_${message?.message}_ \n\nAcesse https://lovebox.khaue.com.br e envie também para alguém que você goste!`
-        })
-    }).then(response => response.json())
-        .then(json => {
-            response = json.status == 'success'
-        })
-
-    // body: `Você é uma pessoa muito especial! 💕💕 \nAlguém enviou um *correio elegante* para você 😍😍 com a seguinte mensagem:\n\n _${message?.message}_ \n\nAcesse https://lovebox.khaue.com.br e envie também para alguém que você goste!`,
-    // body: `Como é bom ser lembrado por alguém especial! 😍 \nVocê acabou de receber um Correio Elegante. 💘 \nConfira sua mensagem 👇 \n\n_${message?.message}_ \n\nAcesse https://lovebox.khaue.com.br e envie também para alguém que você goste!`,
-    // body: `Como é bom ser lembrado por alguém especial! 😍 \nVocê acabou de receber um Correio Elegante. 💘 \n Para conferir a mensagem recebida envie SIM 👇 \n\nAcesse https://lovebox.khaue.com.br e envie também para alguém que você goste!`,
-
-    return response;
-}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Object>) {
     if (req.method !== 'POST') {
